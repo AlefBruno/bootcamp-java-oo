f7fea69d5b93405cee424e4df6118381fa1fe01e
Alef Rodrigues <alefbruno@Outlook.com>
2022-11-09 19:24:14 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.bootcamp.desafio.oo.Curso;
+import br.com.bootcamp.desafio.oo.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Curso curso = new Curso();
+
+    curso.setDescricao("Java");
+    curso.setTitulo("Java");
+    curso.setCargaHoraria(9);
+    System.out.println(curso);
+
+    Mentoria mentoria = new Mentoria();
+
+    mentoria.setTitulo("Java");
+    mentoria.setDescricao("Mentor Java");
+    mentoria.setData(LocalDate.now());
+    System.out.println(mentoria);
+
   }
 }
